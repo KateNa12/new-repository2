1eef923f44f0cbb2d0693570171adc67c8bf7472
Kateryna Naimark <kateryna.naimark@gmail.com>
2024-11-17 13:12:19 +0200

    hasky added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,17 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ['**/*.{*.js}']},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{*.js}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  {rules:{
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'], // Enforce single quotes for strings
-    indent: ['error', 2], // Enforce 2-space indentation for better readability
-    'comma-dangle': ['error', 'always-multiline'], // Require trailing commas in multiline lists/objects
-  }},
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'], // Enforce single quotes for strings
+      indent: ['error', 2], // Enforce 2-space indentation for better readability
+      'comma-dangle': ['error', 'always-multiline'], // Require trailing commas in multiline lists/objects
+    },
+  },
 ];
